013a3d63c4ba1723294e931a38f11aee4a6173f8
Jack Arthur Harrhy <me@jackharrhy.com>
2019-12-03 19:58:49 -0330

    day2, part2

---
+++ day2/day2-part2.cr
@@ -0,0 +1,44 @@
+m = [] of Int32
+
+STDIN.each_line do |line|
+  line.split(",").each do |i|
+    m.push i.to_i32
+  end
+end
+
+def run(m)
+  c = 0
+
+  until m[c] == 99
+    p1 = m[c+1]
+    p2 = m[c+2]
+    p3 = m[c+3]
+
+    case m[c]
+    when 1
+      m[p3] = m[p1] + m[p2]
+    when 2
+      m[p3] = m[p1] * m[p2]
+    end
+
+    c += 4
+  end
+
+  m[0]
+end
+
+possible_nouns = 0..99
+possible_verbs = 0..99
+
+possible_nouns.each do |noun|
+  possible_verbs.each do |verb|
+    mcopy = m.clone
+    mcopy[1] = noun
+    mcopy[2] = verb
+    result = run(mcopy)
+    if result == 19690720
+      puts 100 * noun + verb
+      exit 0
+    end
+  end
+end

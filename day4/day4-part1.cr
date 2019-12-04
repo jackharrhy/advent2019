c19cea156cccb10f3c664c6c2c45246831c26c65
Jack Arthur Harrhy <me@jackharrhy.com>
2019-12-04 12:57:32 -0330

    day4, part1

---
+++ day4/day4-part1.cr
@@ -0,0 +1,25 @@
+range = 347312..805915
+
+found = 0
+
+range.each do |num|
+  fail = false
+  double_exists = false
+  last = -1
+
+  num.to_s.chars.each do |n_as_char|
+    next if fail
+
+    n = n_as_char.to_i
+
+    double_exists = true if last == n
+    fail = true if last > n
+    last = n
+  end
+
+  fail = true if !double_exists
+
+  found += 1 if !fail 
+end
+
+puts found

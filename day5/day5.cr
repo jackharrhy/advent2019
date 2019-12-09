5420005a500a89362e57bea2473ae1bb9401b2fe
Jack Arthur Harrhy <me@jackharrhy.com>
2019-12-09 18:54:37 -0330

    day5, part1

--- day5/day5.cr
+++ day5/day5.cr
@@ -6,20 +6,39 @@ STDIN.each_line do |line|
   end
 end
 
+def debug
+  debug = ENV["DEBUG"] ||= "false"
+  debug === "true"
+end
+
 args = [1]
 
 def run(m, args)
+  d = debug
   c = 0
 
-  until m[c] == 99
-    # parse out the opcode, the right-most two digits
-    instructs = m[0].to_s
-    opcode = instructs[instructs.size - 2, instructs.size].to_i
+  opcode_map = {
+    1 => 4,
+    2 => 4,
+    3 => 2,
+    4 => 2,
+  }
 
-    # parse the remaining mode instructions, including the omitted param
+  until m[c] == 99
+    puts "\nc: #{c} - #{m[c,6]}..." if d
+
+    # parse out the opcode, the right-most two digits
+    instructs = m[c].to_s
+
+    opcode = instructs[instructs.size - 2, instructs.size].to_i
+    raise "opcode more than 4!" if opcode > 4
+
+    opcode_jump = opcode_map[opcode]
+
+    # parse the remaining mode instructions, including the potential omitted param
     modes = instructs.chars.map { |m| m.to_i }
     modes.pop 2
-    modes.unshift 0 if modes.size == 2
+    modes.reverse!
 
     # make a note of both the actual positions and values of the items
     positions = [] of Int32
@@ -28,32 +47,34 @@ def run(m, args)
     (1..3).each do |x|
       param = m[c + x]?.is_a?(Int32) ? m[c + x] : 0
       positions << param
+
+      # still handle one-int opcodes, and also
       # resolve real location if not in immediate mode
-      param = m[param] if modes[x - 1] == 0
+      mode = modes[x - 1]?
+      if modes.size === 0 || mode == 0 || mode == nil
+        param = m[param]?.is_a?(Int32) ? m[param] : 0
+      end
+
       params << param
     end
 
-    p1, p2, p3 = positions
-    pr1, pr2, pr3 = params
+    puts "instructs: #{instructs}, opcode: #{opcode}" if d
+    puts "modes: #{modes}, positions: #{positions}, params: #{params}" if d
 
     case opcode
     when 1
-      m[p3] = pr1 + pr2
-      c += 4
+      m[positions[2]] = params[0] + params[1]
     when 2
-      m[p3] = pr1 * pr2
-      c += 4
+      m[positions[2]] = params[0] * params[1]
     when 3
       int = args.shift
-      m[p1] = int
-      c += 2
+      m[positions[0]] = int
     when 4
-      puts pr1
-      c += 2
+      puts params[0]
     end
-  end
 
-  pp m
+    c += opcode_jump
+  end
 end
 
 run(m, args)
